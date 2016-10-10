ce772bc80368267ee0ab9a8c84eaf17717c75e18
Alexander Parfenov <asp437@gmail.com>
2016-10-10 21:57:07 +0300

    Init Commit. Perlin terrain generator port.

---
+++ Assets/CityGenerator/Scripts/Core/CityGenerator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CityGenerator : MonoBehaviour {
+    public TerrainGenerator terrainGenerator;
+    public int terrainGeneratorSeed;
+    public AbstractAgent[] agentsList;
+
+    // Use this for initialization
+    void Start () {
+        terrainGenerator.GenerateTerrain(terrainGeneratorSeed, null);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
